0e5b879de1a8875b5f3c440c7f04d2707a4f0a14
Daniel Cerón <damusan_4@hotmail.es>
2020-07-01 19:59:35 -0500

    si

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import classes from "./App.module.css";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	textButtom = "showPersons";
+
+	state = {
+		persons: [
+			{ id: "sas", name: "max", age: 28 },
+			{ id: "dasd", name: "Manndahsdj", age: 26 },
+			{ id: "adsdasd", name: "si", age: 63 },
+		],
+		showPersons: false,
+	};
+
+	deletePersonHandler = (index) => {
+		const persons = [...this.state.persons];
+		persons.splice(index, 1);
+		this.setState({ persons });
+	};
+
+	nameChangeHandler = (event, id) => {
+		const personIndex = this.state.persons.findIndex((p) => {
+			return p.id === id;
+		});
+
+		const persons = [...this.state.persons];
+		persons[personIndex].name = event.target.value;
+		this.setState({ persons });
+	};
+
+	togglePersonsHandler = () => {
+		const doesShow = this.state.showPersons;
+
+		this.setState({ showPersons: !doesShow });
+		!doesShow ? (this.textButtom = "hide Persons") : (this.textButtom = "ShowPersons");
+	};
+
+	render() {
+		let persons = null;
+		let btnClasses = "";
+
+		if (this.state.showPersons) {
+			persons = (
+				<div>
+					{this.state.persons.map((person, index) => {
+						return (
+							<Person
+								click={this.deletePersonHandler.bind(this, index)}
+								changed={(event) => this.nameChangeHandler(event, person.id)}
+								name={person.name}
+								age={person.age}
+								key={person.id}></Person>
+						);
+					})}
+				</div>
+			);
+			btnClasses = classes.Red;
+		}
+
+		let AssignedClasses = [];
+		if (this.state.persons.length <= 2) {
+			AssignedClasses.push(classes.red);
+		}
+		if (this.state.persons.length <= 1) {
+			AssignedClasses.push(classes.bold);
+		}
+
+		//* el bonito return :D
+		return (
+			<div className={classes.App}>
+				<h1> im the title</h1>
+				<p className={AssignedClasses.join(" ")}>hi, i'm the subTitle </p>
+				<button className={btnClasses} onClick={this.togglePersonsHandler}>
+					{this.textButtom}
+				</button>
+				{persons}
+			</div>
+		);
+	}
 }
 
 export default App;
